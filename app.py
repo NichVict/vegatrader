a30aff75035b1e434bfb30f4a03aa39499405878
Vanderlei Alexandre Gasparini <138238431+NichVict@users.noreply.github.com>
2025-10-09 21:30:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ No menu lateral, você pode escolher entre:
 - **CARTEIRA CURTO PRAZO**
 - **LOSS CURTÍSSIMO**
 - **LOSS CURTO**
-- **CLUBE STOP**
+- **CLUBE**
 - **LOSS CLUBE**
 
 Cada página monitora preços, tempos acumulados e envia alertas automáticos.
@@ -19,6 +19,7 @@ Cada página monitora preços, tempos acumulados e envia alertas automáticos.
 st.subheader("Acessos rápidos")
 st.page_link("pages/curtissimo.py", label="⚡ Curtíssimo Prazo")
 st.page_link("pages/curto.py", label="⚡ Curto Prazo")
+st.page_link("pages/clube.py", label="💼 Clube")
 st.page_link("pages/loss_curtissimo.py", label="🛑 Loss Curtíssimo")
 st.page_link("pages/loss_curto.py", label="🚨 Loss Curto")
 st.page_link("pages/loss_clube.py", label="💥 Loss Clube")
