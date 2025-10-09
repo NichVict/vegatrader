aa522a0c793cf510786c434c7039656869350ed2
Vanderlei Alexandre Gasparini <138238431+NichVict@users.noreply.github.com>
2025-10-09 21:38:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,24 @@ st.markdown("""
 Bem-vindo ao **Painel Principal**.
 
 No menu lateral, você pode escolher entre:
-- **CARTEIRA CURTÍSSIMO PRAZO**
+
 - **CARTEIRA CURTO PRAZO**
-- **LOSS CURTÍSSIMO**
-- **LOSS CURTO**
+- **CARTEIRA CURTÍSSIMO PRAZO**
 - **CLUBE**
+- **LOSS CURTO PRAZO**
+- **LOSS CURTÍSSIMO PRAZO**
 - **LOSS CLUBE**
 
 Cada página monitora preços, tempos acumulados e envia alertas automáticos.
 """)
 # (opcional) atalhos clicáveis para as páginas:
 st.subheader("Acessos rápidos")
-st.page_link("pages/curtissimo.py", label="⚡ Curtíssimo Prazo")
+
 st.page_link("pages/curto.py", label="⚡ Curto Prazo")
-st.page_link("pages/clube.py", label="💼 Clube")
-st.page_link("pages/loss_curtissimo.py", label="🚨 Loss Curtíssimo")
+st.page_link("pages/curtissimo.py", label="⚡ Curtíssimo Prazo")
+st.page_link("pages/clube.py", label="⚡ Clube")
 st.page_link("pages/loss_curto.py", label="🚨 Loss Curto")
+st.page_link("pages/loss_curtissimo.py", label="🚨 Loss Curtíssimo")
 st.page_link("pages/loss_clube.py", label="🚨 Loss Clube")
 
 
